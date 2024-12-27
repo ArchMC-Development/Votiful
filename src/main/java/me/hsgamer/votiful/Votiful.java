b8d5c6f4174c44d3ea401e5b9d5cb837e9ed4133
hsgamer <huynhqtienvtag@gmail.com>
2024-12-27 22:59:50 +0700

    register config

--- src/main/java/me/hsgamer/votiful/Votiful.java
+++ src/main/java/me/hsgamer/votiful/Votiful.java
@@ -1,8 +1,11 @@
 package me.hsgamer.votiful;
 
 import io.github.projectunified.minelib.plugin.base.BasePlugin;
+import me.hsgamer.hscore.bukkit.config.BukkitConfig;
+import me.hsgamer.hscore.config.proxy.ConfigGenerator;
 import me.hsgamer.topper.spigot.storage.simple.SpigotDataStorageBuilder;
 import me.hsgamer.topper.storage.simple.builder.DataStorageBuilder;
+import me.hsgamer.votiful.config.MainConfig;
 import me.hsgamer.votiful.listener.VoteListener;
 import me.hsgamer.votiful.manager.VoteManager;
 
@@ -14,6 +17,8 @@ public final class Votiful extends BasePlugin {
     protected List<Object> getComponents() {
         return Arrays.asList(
                 new DataStorageBuilder(),
+
+                ConfigGenerator.newInstance(MainConfig.class, new BukkitConfig(this)),
 
                 new VoteManager(this),
 
