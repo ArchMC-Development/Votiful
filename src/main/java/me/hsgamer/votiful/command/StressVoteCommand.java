01a31794ceb37053d77bce449c76ef5a9073e0d3
hsgamer <huynhqtienvtag@gmail.com>
2025-01-03 22:19:18 +0700

    stress vote for multiple names

--- src/main/java/me/hsgamer/votiful/command/StressVoteCommand.java
+++ src/main/java/me/hsgamer/votiful/command/StressVoteCommand.java
@@ -5,12 +5,13 @@ import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.net.VotifierSession;
 import io.github.projectunified.minelib.scheduler.async.AsyncScheduler;
 import me.hsgamer.votiful.Votiful;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collections;
-import java.util.stream.IntStream;
+import java.util.function.Supplier;
 
 public class StressVoteCommand extends Command {
     private final Votiful plugin;
@@ -39,11 +40,34 @@ public class StressVoteCommand extends Command {
         }
         int finalAmount = amount;
         NuVotifierBukkit nuVotifierBukkit = JavaPlugin.getPlugin(NuVotifierBukkit.class);
+        Supplier<String> nameProvider = () -> {
+            if (name.equalsIgnoreCase("@r")) {
+                int maxNumber = 1000;
+                if (args.length > 2) {
+                    try {
+                        maxNumber = Integer.parseInt(args[2]);
+                    } catch (NumberFormatException ignored) {
+                        // IGNORED
+                    }
+                }
+                return "TestVotePlays" + (int) (Math.random() * maxNumber);
+            } else if (name.equalsIgnoreCase("@e")) {
+                OfflinePlayer[] players = plugin.getServer().getOfflinePlayers();
+                return players[(int) (Math.random() * players.length)].getName();
+            } else {
+                return name;
+            }
+        };
         AsyncScheduler.get(plugin).run(() -> {
-            IntStream.rangeClosed(1, finalAmount)
-                    .parallel()
-                    .mapToObj(i -> new Vote("TestVote", name, "127.0.0.1", Long.toString(System.currentTimeMillis(), 10)))
-                    .forEach(vote -> nuVotifierBukkit.onVoteReceived(vote, VotifierSession.ProtocolVersion.TEST, "localhost.test"));
+            for (int i = 0; i < finalAmount; i++) {
+                String finalName = nameProvider.get();
+                if (finalName == null) {
+                    sender.sendMessage("Invalid name");
+                    return;
+                }
+                Vote vote = new Vote("TestVote", finalName, "127.0.0.1", Long.toString(System.currentTimeMillis(), 10));
+                nuVotifierBukkit.onVoteReceived(vote, VotifierSession.ProtocolVersion.TEST, "localhost.test");
+            }
             sender.sendMessage("Sent " + finalAmount + " vote(s) to " + name);
         });
         return true;
